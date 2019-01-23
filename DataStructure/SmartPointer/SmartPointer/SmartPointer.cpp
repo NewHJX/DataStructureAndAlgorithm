9d556c406892e452fdd6f24e24ec65b2dfa081a3
hjx <junxiong_he@qq.com>
2019-01-23 08:58:08 +0800

    修改SmartPointer为泛型

--- DataStructure/SmartPointer/SmartPointer/SmartPointer.cpp
+++ DataStructure/SmartPointer/SmartPointer/SmartPointer.cpp
@@ -13,20 +13,23 @@ private:
 
 };
 
+template<typename T>
+class SmartPt;
 
+template<class Type>
 class U_Ptr {
 private:
-	friend class SmartPt;
-	U_Ptr(Point *ptr) :p(ptr), count(1) {}
+	friend class SmartPt<Type>;
+	U_Ptr(Type *ptr) :p(ptr), count(1) {}
 	~U_Ptr() { delete p; }
 	int count;
-	Point *p;
+	Type *p;
 };
 
-
+template<typename T>
 class SmartPt {
 public:
-	SmartPt(Point *ptr):rp(new U_Ptr(ptr)){}
+	SmartPt(T *ptr):rp(new U_Ptr<T>(ptr)){}
 	SmartPt(const SmartPt &sp) :rp(sp.rp) { ++rp->count; }
 	SmartPt& operator=(const SmartPt& rhs) {
 		++rhs.rp->count;
@@ -42,7 +45,7 @@ public:
 			std::cout << "还有" << rp->count << "个指针指向基础对象" << std::endl;
 	}
 private:
-	U_Ptr *rp;
+	U_Ptr<T> *rp;
 };
 
 int main()
@@ -54,11 +57,11 @@ int main()
 	//使用花括号控制三个指针指针的生命期，观察计数的变化
 
 	{
-		SmartPt sptr1(pa);//此时计数count=1
+		SmartPt<Point> sptr1(pa);//此时计数count=1
 		{
-			SmartPt sptr2(sptr1); //调用复制构造函数，此时计数为count=2
+			SmartPt<Point> sptr2(sptr1); //调用复制构造函数，此时计数为count=2
 			{
-				SmartPt sptr3 = sptr1; //调用赋值操作符，此时计数为conut=3
+				SmartPt<Point> sptr3 = sptr1; //调用赋值操作符，此时计数为conut=3
 			}
 			//此时count=2
 		}
@@ -67,7 +70,6 @@ int main()
 	//此时count=0；pa对象被delete掉
 
 	std::cout << pa->getX() << std::endl;
-
 	getchar();
 	return 0;
 }
